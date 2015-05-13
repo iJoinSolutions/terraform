7e4358c57b75585142b5aa0aed661c0eb170a30b
Clint Shryock <clint@ctshryock.com>
2015-05-13 16:53:12 -0500

    remove default here

--- builtin/providers/aws/resource_aws_db_instance.go
+++ builtin/providers/aws/resource_aws_db_instance.go
@@ -90,7 +90,6 @@ func resourceAwsDbInstance() *schema.Resource {
 				Type:     schema.TypeInt,
 				Optional: true,
 				Computed: true,
-				Default:  1,
 			},
 
 			"backup_window": &schema.Schema{
