a17f7a33ceb316b389a6fe0cf5823bd53149b78b
Michael Austin <m_austin@me.com>
2015-05-21 15:33:30 -0400

    working object provider

--- builtin/providers/aws/resource_aws_s3_bucket_object.go
+++ builtin/providers/aws/resource_aws_s3_bucket_object.go
@@ -1,10 +1,15 @@
 package aws
 
 import (
+	"os"
+	"fmt"
+	"log"
+
 	"github.com/hashicorp/terraform/helper/schema"
 
 	"github.com/awslabs/aws-sdk-go/aws"
 	"github.com/awslabs/aws-sdk-go/service/s3"
+	"github.com/awslabs/aws-sdk-go/aws/awsutil"
 )
 
 func resourceAwsS3BucketObject() *schema.Resource {
@@ -38,6 +43,31 @@ func resourceAwsS3BucketObject() *schema.Resource {
 }
 
 func resourceAwsS3BucketObjectCreate(d *schema.ResourceData, meta interface{}) error {
+	s3conn := meta.(*AWSClient).s3conn
+
+	bucket := d.Get("bucket").(string)
+	key    := d.Get("key").(string)
+	source := d.Get("source").(string)
+
+	file, err := os.Open(source)
+
+	if err != nil {
+		return fmt.Errorf("Error opening S3 bucket object source(%s): %s", source, err)
+	}
+
+	resp, err := s3conn.PutObject(
+		&s3.PutObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(key),
+			Body:   file,
+		})
+
+	if err != nil {
+		return fmt.Errorf("Error putting object in S3 bucket (%s): %s", bucket, err)
+	}
+
+	log.Printf(awsutil.StringValue(resp))
+	d.SetId(*resp.ETag)
 	return nil
 }
 
