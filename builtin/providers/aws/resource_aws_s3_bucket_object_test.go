1d6358f3f7c152770b57af49ae5f44618c954107
Michael Austin <m_austin@me.com>
2015-05-22 19:36:06 -0400

    temp file is created but not destroyed yet

--- builtin/providers/aws/resource_aws_s3_bucket_object_test.go
+++ builtin/providers/aws/resource_aws_s3_bucket_object_test.go
@@ -3,18 +3,25 @@ package aws
 import (
 	"testing"
 	"fmt"
-//	"log"
-
+	"io/ioutil"
+	"os"
+	"log"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 
 	"github.com/awslabs/aws-sdk-go/aws"
 	"github.com/awslabs/aws-sdk-go/service/s3"
 )
-
+var tf,err = ioutil.TempFile("", "tf")
+var _ = log.Printf
 func TestAccAWSS3BucketObject_basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
+		PreCheck:     func() { 
+			if err != nil {
+				panic(err)
+			} 
+			testAccPreCheck(t)
+		},
 		Providers: testAccProviders,
 		CheckDestroy: testAccCheckAWSS3BucketObjectDestroy,
 		Steps: []resource.TestStep{
@@ -29,24 +36,22 @@ func TestAccAWSS3BucketObject_basic(t *testing.T) {
 }
 
 func testAccCheckAWSS3BucketObjectDestroy(s *terraform.State) error {
-	s3conn := testAccProvider.Meta().(*AWSClient).s3conn
+	//s3conn := testAccProvider.Meta().(*AWSClient).s3conn
 
 	for _, rs := range s.RootModule().Resources {
 		if rs.Type != "aws_s3_bucket_object" {
 			continue
 		}
 		
-		_, err := s3conn.GetObject(
-			&s3.GetObjectInput{
-				IfMatch: aws.String(rs.Primary.ID),
-		})
-		return fmt.Errorf("get object err %s",err)
 	}
-	return fmt.Errorf("no resources")
+	return nil
 }
 
 func testAccCheckAWSS3BucketObjectExists(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
+
+		defer os.Remove(tf.Name());
+
 		rs, ok := s.RootModule().Resources[n]
 		if !ok {
 			return fmt.Errorf("Not Found: %s", n)
@@ -59,6 +64,8 @@ func testAccCheckAWSS3BucketObjectExists(n string) resource.TestCheckFunc {
 		s3conn := testAccProvider.Meta().(*AWSClient).s3conn
 		_, err := s3conn.GetObject(
 			&s3.GetObjectInput{
+				Bucket:  aws.String(rs.Primary.Attributes["bucket"]),
+				Key:     aws.String(rs.Primary.Attributes["key"]),
 				IfMatch: aws.String(rs.Primary.ID),
 		})
 		if err != nil {
@@ -67,6 +74,7 @@ func testAccCheckAWSS3BucketObjectExists(n string) resource.TestCheckFunc {
 		return nil
 	}
 }
+
 var randomBucket = randInt;
 var testAccAWSS3BucketObjectConfig = fmt.Sprintf(`
 resource "aws_s3_bucket" "bucket" {
@@ -76,7 +84,7 @@ resource "aws_s3_bucket_object" "object" {
 	depends_on = "aws_s3_bucket.bucket"
 	bucket = "tf-object-test-bucket-%d"
 	key = "test-key"
-	source = "test.txt"
+	source = "%s"
 }
-`, randomBucket, randomBucket)
+`, randomBucket, randomBucket, tf.Name())
 
